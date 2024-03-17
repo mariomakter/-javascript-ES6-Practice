fccf6f016aef426df5b4bc488ad02d1ef6ae1960
mariomakter <mariom9267idb@gmail.com>
2024-03-17 22:36:35 +0600

    Best practice javascript ES6

---
+++ variables.js
@@ -0,0 +1,17 @@
+
+// if(a>b){
+//     let a = 12;
+//     let b = 20
+//     console.log('a is greter than b');
+// }
+// else{
+//     console.log('b is largest');
+// }
+// for(var i = 0; i<5; i++){
+//     console.log("inside loop" +i);
+// }
+// console.log('outside.log' +i);
+for(let i = 0; i<5; i++){
+    console.log("inside loop" +i);
+}
+console.log('outside.log' +i);
